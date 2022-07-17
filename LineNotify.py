a406e5217c474550102a93d2756bbfbe26ad3ecc
BeeXii <109472287+BeeXii@users.noreply.github.com>
2022-07-18 00:50:36 +0700

    Update LineNotify.py

--- LineNotify.py
+++ LineNotify.py
@@ -4,7 +4,7 @@ import requests
 class _lineAPI:
     def __init__(self) -> None:
         self.BaseuRL = 'https://notify-api.line.me/api/notify'
-        self.token = 'Change -> Token'
+        self.token = '3lCRgcVFMC2mnLw6PQqq8voNK9udGuqq9MZPo1O7jOe'
         self.headers  = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+ self.token }
 
         self.req = requests.session()
